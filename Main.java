2c49a1ba54eb4432dbfcfe70fa21da3bc30fcdf4
Eber <eberchiecher2@gmail.com>
2022-04-27 20:34:41 -0300

    segundo commit

--- Main.java
+++ Main.java
@@ -1,6 +1,29 @@
+include.java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola probando.");
-        
+        Scanner entrada= new Scanner(System.in); 
+       int n=2;
+       int boton;
+       System.out.println("se ejecuta");
+
+       while(n==2){
+        introduccion();
+
+        System.out.println("");
+        n=3;
+
+
+       }
+     
     }
+    public static void introduccion(){
+
+            System.out.println("###########################################################################");
+            System.out.println("Bienvenido a nuestro proyecto");
+            System.out.println("###########################################################################");
+            System.out.println("1:Ingresar ");
+            System.out.println("2:Eliminar");
+            System.out.println("3: Ver");
+    }
+    
 }
