44d0376bd5885356f57c634d03e3559b317e9f19
pabloesanabriaq <pabloesanabriaq@gmail.com>
2022-05-13 19:10:13 -0300

    Comenzando la función de dispersión

---
+++ HashDispersion.java
@@ -0,0 +1,9 @@
+public class HashDispersion {
+    //Utilizaremos Aritmética Modular
+    //n es el tamaño de la cantidad de claves a dispersar
+    //m es el tamaño de la tabla de dispersión
+    int n = 1000, m;
+    double factorCarga = Math.round(n / 0.8);
+
+    
+}
