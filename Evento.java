1aabfe641e80d4fdd6455eb086b7ea866924c528
GerN7 <you@example.com>
2022-05-13 11:55:08 -0300

    <String> en ArrayList agregado

--- Evento.java
+++ Evento.java
@@ -6,7 +6,7 @@ public class Evento extends Agenda{
     protected LocalDateTime fechaFin;
     ArrayList<String> invitados;
 
-    Evento(int id, String nombre, String descripcion, String lugar, ArrayList invitados, LocalDateTime fechaInicio, LocalDateTime fechaFin){
+    Evento(int id, String nombre, String descripcion, String lugar, ArrayList<String> invitados, LocalDateTime fechaInicio, LocalDateTime fechaFin){
         super(id, nombre, descripcion, lugar, fechaInicio);
         this.fechaFin = fechaFin;
         this.invitados = invitados;
--- Tareas.java
+++ Tareas.java
@@ -1,17 +1,18 @@
-//clase hijo tareas 
-import java.time.LocalTime;
+//clase Hijo - Tareas 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Tareas extends Agenda{
-   protected LocalDateTime fechaFin;
-   ArrayList<String> tareas; 
-   Tareas(int id, String nombre, String descripcion,String lugar, LocalDateTime fechaInicio, LocalDateTime fechaFin, ArrayList<String> tareas ){
-       super(id, nombre,  descripcion,lugar,  fechaInicio);///estaban declaradas asi que le saque la declaracion.Eber 
-       this.fechaFin = fechaFin;
-       this.tareas = tareas;
-   }
-    public LocalDateTime getFechaFin(){///otra correccion que me falto, cambiar date por localdatetime 
+    protected LocalDateTime fechaFin;
+    ArrayList<String> tareas;
+    
+    Tareas(int id, String nombre, String descripcion, String lugar, LocalDateTime fechaInicio, LocalDateTime fechaFin, ArrayList<String> tareas){
+        super(id, nombre,  descripcion,lugar,  fechaInicio);
+        this.fechaFin = fechaFin;
+        this.tareas = tareas;
+    }
+
+    public LocalDateTime getFechaFin(){
         return this.fechaFin;
     }
 }
