50c952377725707c95f0990b79cf4be38854e207
eber <eberchiecher2@gmail.com>
2022-05-13 18:01:34 -0300

    codigo funcional sin controles, y desordenado

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int hay_elemento=0;
-        Scanner entrada = new Scanner(System.in); 
+        Scanner entrada = new Scanner(System.in); ///indice para eliminar elementos tipo agenda, evento,tareas
         ArrayList<Agenda> lista_recordatorio=new ArrayList<>();///arraylist donde se almacena elementos tipo agenda
         ArrayList<Evento> lista_evento= new ArrayList<>();///Arraylist donde se almacena elementos tipos evento
         ArrayList<Tareas> lista_tareas=new ArrayList<>();///Arraylist donde se almacenan elementos tipos tareas
@@ -25,10 +25,10 @@ public class Main {
         id_evento = 0;
         int dia, mes, año;///dia mes año que le pido al usuario
         int horas, minutos;///horas y minutos que le pido al usuario
-        int cantidad_tareas;///la cantidad de tareas que va almacenar e usuario en la arraylist tareas de la clase tareas
+        int cantidad_tareas;///esta variable tambien se usa para la cantidad de invitados
         LocalDate fecha;
         LocalTime hora= LocalTime.of(0,0);
-        LocalDateTime c= LocalDateTime.now();
+       
         
         
 
@@ -58,6 +58,7 @@ public class Main {
                     entrada.nextLine();
                         switch (opcion){
                             case 1:
+                                ///TAREA
                                 System.out.println("Dime el titulo del conjunto de tareas: ");
                                 nombre = entrada.nextLine();
                                 System.out.println("Dime la descripcion del conjunto de tareas");
@@ -116,23 +117,60 @@ public class Main {
             
                                 break;
                             case 2:
-                                
+                                ////EVENTO
+                                System.out.println("Dime el titulo del evento: ");
+                                nombre = entrada.nextLine();
+                                System.out.println("Dime la descripcion del evento");
+                                descripcion = entrada.nextLine();
+                                System.out.println("Dime el lugar donde se realizara el evento");///capaz que no va para recordatorio
+                                lugar = entrada.nextLine();
+                                System.out.println("Dime la fecha de inicio del evento:");
+                                System.out.println("año");
+                                año = entrada.nextInt();
+                                entrada.nextLine();
+                                System.out.println("mes");
+                                mes = entrada.nextInt();
+                                entrada.nextLine();
+                                System.out.println("dia");
+                                dia = entrada.nextInt();
+                                entrada.nextLine();
+                                fecha = LocalDate.of(año,mes,dia);///pongo la fecha ingresada en un variable localdate
+                                fecha_inicio=LocalDateTime.of(fecha,hora);
+                                System.out.println("Dime el dia en que termina el evento:");
+                                System.out.println("año");
+                                año = entrada.nextInt();
+                                entrada.nextLine();
+                                System.out.println("mes");
+                                mes = entrada.nextInt();
+                                entrada.nextLine();
+                                System.out.println("dia");
+                                dia = entrada.nextInt();
+                                entrada.nextLine();
+                                fecha = LocalDate.of(año,mes,dia);
+                                System.out.println("Dime la hs de finalizacion del evento");
+                                System.out.println("HS: ");
+                                horas= entrada.nextInt();
+                                entrada.nextLine();
+                                System.out.println("Minutos: ");
+                                minutos=entrada.nextInt();
+                                entrada.nextLine();
+                                hora=LocalTime.of(horas,minutos);
+                                fecha_final=LocalDateTime.of(fecha,hora);
+                                System.out.println("Dime la cantidad de invitados que vas a realizar en el periodo de tiempo especificado");
+                                cantidad_tareas=entrada.nextInt();
+                                entrada.nextLine();
+                                for(int i=0; i<cantidad_tareas;i++){
+                                    System.out.println("Invitado numero "+(i+1)+":");
+                                    invitados.add(i ,entrada.nextLine());
+
+                                }
+                                lista_evento.add(new Evento(id_evento, nombre, descripcion, lugar, invitados, fecha_inicio, fecha_final));
+                                id_evento=id_evento+1;
 
                                 break;
                             case 3:
-                                //Como .size() devuelve el tamaño del array puedo usar ese número para el próximo ingreso!
-                                // Si el tamaño es menor al id del último valor agregado (porque se puede haber eliminado algo) entonces usamos ese id como referencia.
-                                ///Eber: lo puse entre comillas porque no lo entiendo
-                                
-                                /*int tamanio = lista_recordatorio.size();
-                                int ultimo_id = lista_recordatorio.indexOf(lista_recordatorio.get(tamanio - 1));
-                                if(tamanio < ultimo_id){
-                                    id_recordatorio = ultimo_id +1;
-                                } else {
-                                    id_recordatorio = lista_recordatorio.size();
-                                }
-                                */
-                                
+                                ///AGENDA 
+                                        
                                 System.out.println("Dime el titulo del recordatorio: ");
                                 nombre = entrada.nextLine();
                                 System.out.println("Dime el recordatorio");
@@ -173,6 +211,11 @@ public class Main {
                                 id_tareas = id_tareas-1;
                                 break;
                             case 2:
+                                System.out.println("Dime el numero del evento, si no estas seguro ve a la opcion ver");
+                                numero_recordatorio = entrada.nextInt();
+                                entrada.nextLine();
+                                lista_evento.remove(numero_recordatorio-1);
+                                id_evento = id_evento-1;
                                 break;
                             case 3:
                                 System.out.println("Dime el numero del recordatorio, si no estas seguro ve a la opcion ver");
@@ -193,6 +236,7 @@ public class Main {
                     entrada.nextLine();
                         switch (opcion){
                             case 1:
+                                ///TAREA
                                 for(int i=0;i<lista_tareas.size();i++){
 
                                     System.out.println("numero de la tarea: "+ (i+1));
@@ -217,8 +261,30 @@ public class Main {
 
                                 break;
                             case 2:
+                                ///EVENTO
+                                for (int i=0; i<lista_evento.size();i++){
+                                System.out.println("numero del evento: "+ (i+1));
+                                System.out.println("Titulo: "+ lista_evento.get(i).getNombre());
+                                System.out.println("Descripcion: "+ lista_evento.get(i).getDescripcion());
+                                System.out.println("Lugar: "+ lista_evento.get(i).getLugar());
+                                System.out.println("Fecha inicio: "+ lista_evento.get(i).getFechaInicio().format(f));
+                                System.out.println("Fecha fin: "+lista_evento.get(i).getFechaFin().format(f));
+                                for(int j=0;j<lista_evento.get(i).invitados.size();j++){
+                                    System.out.println("Invitado numero: "+(j+1));
+                                    System.out.println(lista_evento.get(i).invitados.get(j));
+                                }
+                                System.out.println("");
+                                hay_elemento=1;
+
+                            }
+                            if(hay_elemento==0){
+
+                                System.out.println("Lo sentimos pero no hay elementos tipo evento almacenados");
+                            }
+                            hay_elemento=0;
                                 break;
                             case 3:
+                                ///RECORDATORIO
                                 
                                 for(int i=0; i<lista_recordatorio.size();i++){
                                     System.out.println("numero de recordatorio: "+ (i+1));
