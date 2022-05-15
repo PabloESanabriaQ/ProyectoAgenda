5c9ea07e4e9f600897b9774775aed4d02be765c6
pabloesanabriaq <pabloesanabriaq@gmail.com>
2022-05-15 16:14:07 -0300

    Dispersión utilizando aritmética modular

--- HashDispersion.java
+++ HashDispersion.java
@@ -2,8 +2,12 @@ public class HashDispersion {
     //Utilizaremos Aritmética Modular
     //n es el tamaño de la cantidad de claves a dispersar
     //m es el tamaño de la tabla de dispersión
-    int n = 1000, m;
-    double factorCarga = Math.round(n / 0.8);
+    //El factor de carga es de 0.8. El tamaño de mis datos es de 1000, entonces si m/n = 0.8 => m = n / 0.8 
+    static final int n = 1000, m = 1259;
 
+    static int sad(long id){
+        return Math.toIntExact(id % m);
+    }
+    
     
 }
