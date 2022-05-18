f9de88d18334074ebc89c8779271bd52c7984c04
pabloesanabriaq <pabloesanabriaq@gmail.com>
2022-05-18 00:19:07 -0300

    Agregadas funciones de tamaño tabla y calculo numero primo

--- HashDispersion.java
+++ HashDispersion.java
@@ -3,10 +3,37 @@ public class HashDispersion {
     //n es el tamaño de la cantidad de claves a dispersar
     //m es el tamaño de la tabla de dispersión
     //El factor de carga es de 0.8. El tamaño de mis datos es de 1000, entonces si m/n = 0.8 => m = n / 0.8 
-    static final int n = 1000, m = 1259;
 
-    static int sad(long id){
-        return Math.toIntExact(id % m);
+    static boolean esPrimo(int m){
+        //Vamos a comprobar si un número m es primo.
+        //Primero lo dividimos por 2, 3, 5.
+        //Si eso no se cumple utilizamos la raíz de M (descartando el decimal)
+        //es divisible por los números primos anteriores a su raíz, de ser así NO será primo
+        //de lo contrario, es primo.
+        int raizDeM = (int) Math.sqrt(m);
+        if(m % 5 == 0 || m % 3 == 0 || m % 2 == 0){
+            return false;
+        }
+        for(int i = 2; i < raizDeM; i++){
+            if(m % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int calcularTamanioTabla(long n){
+        Double factorDeCarga = 0.8;
+        Double m = n / factorDeCarga;
+        int mEntero = (int) Math.round(m);
+        while (!esPrimo(mEntero)){
+            mEntero++;
+        }
+        return mEntero;
+    }
+
+    static int dispersarClave(long clave, int m){
+        return Math.toIntExact(clave % m);
     }
     
     
