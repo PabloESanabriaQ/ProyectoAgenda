a88c5249f8599962585bdc8019c33d032cd50e0a
pabloesanabriaq <pabloesanabriaq@gmail.com>
2022-05-23 14:00:42 -0300

    Adaptado a n digitos

--- MitadDelCuadrado.java
+++ MitadDelCuadrado.java
@@ -6,17 +6,22 @@ public class MitadDelCuadrado {
     //Así, si el rango es de 0 ... 999 se extraen tres dígitos.
     // SIEMPRE LOS QUE ESTÁN EN LAS MISMAS POSICIONES.
 
-    static int dispersarClave(Double clave){
+    static int dispersarClave(Double clave, int m){
         String n;
-        int claveFinal;
+        int claveFinal, cantidadDigitos;
         //Elevo la clave al cuadrado, luego la redondeo para eliminar decimales.
+        cantidadDigitos = Integer.toString(m).length();
         clave*= clave;
         Math.round(clave);
         //La paso a string para poder manipularla.
         n = clave.toString();
         //Uso substring para seleccionar los dígitos que quiero utilizar
-        //(3 dígitos) y luego lo parseo a int para obtener mi clave final.
-        claveFinal = Integer.parseInt(n.substring(4, 7));
+        //(cantidad digitos dictados por la longitud de m) 
+        //y luego lo parseo a int para obtener mi clave final.
+        claveFinal = Integer.parseInt(n.substring(4, cantidadDigitos));
+        if(claveFinal > m){
+            claveFinal-=m;
+        }
         return claveFinal;
     }
 }
